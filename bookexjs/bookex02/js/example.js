ba0bcb4680659b9e4e921248e2d04180c491a01e
David <kobe40112@gmail.com>
2020-04-03 13:51:19 +0800

    example

---
+++ bookexjs/bookex02/js/example.js
@@ -0,0 +1,13 @@
+//為歡迎訊息建立一個變數
+let greeting = 'Hello';
+let name = 'Kris';
+let message = ', please check your order :';
+//連接以上三個變數來建立歡迎訊息
+let welocome = greeting + name + message ;
+
+//建立變數儲存名牌資訊
+let sign = 'Montague House';
+let tiles = sign.length;
+let subTotal = tiles * 5;
+let shipping = 7;
+let grandTotal = subTotal + shipping;
---
+++ bookexjs/bookex02/js/string-operator.js
@@ -0,0 +1,9 @@
+//使用字串運算子
+
+let greeting = 'Hello';
+let name = 'Kris';
+
+let welcomeMessage = greeting + name + '!';
+
+let el = document.getElementById('greeting');
+el.textContent = welcomeMessage;
